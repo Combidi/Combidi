a1f4a4791149781bec000c5cdeffd9db00102236
Peter Combee <petercombee@gmail.com>
2021-06-04 14:47:52 +0200

    Adding xcode project

---
+++ Shared/CombidiApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombidiApp.swift
+//  Shared
+//
+//  Created by Peter Combee on 04/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct CombidiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
